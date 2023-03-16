3b658f0f1de1bf9029bd7804fd5cc07a477d2ff6
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 14:37:16 +0100

    ___

--- components/DynamicButton.tsx
+++ components/DynamicButton.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const DynamicButton = () => {
+const DynamicButton = ({ onClick }: any) => {
   return (
-    <button className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed">
+    <button
+      className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed"
+      onClick={onClick}
+    >
       Copy text
     </button>
   );
