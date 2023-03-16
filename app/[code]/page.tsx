dcd22da67fe16da73e44a6a49d64f52cc86281f5
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 13:17:54 +0100

    ___

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const TextPage = () => {
-  return <div>TextPage</div>;
+  return (
+    <div className="my-4  flex-col flex items-center justify-center">
+      <textarea
+        className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
+        placeholder="Type here..."
+        // onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button
+        className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed"
+        // onClick={createText}
+        // disabled={!text}
+      >
+        Create
+      </button>
+    </div>
+  );
 };
 
 export default TextPage;
