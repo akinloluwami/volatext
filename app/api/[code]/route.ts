19c1a554fcf3f2d36bc1c405dd5df2cb98e2297e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 12:00:13 +0100

    added the proper type

--- app/api/[code]/route.ts
+++ app/api/[code]/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: any) {
+export async function GET(
+  request: Request,
+  { params }: { params: { code: string } }
+) {
   const code = params.code;
   return NextResponse.json({ hi: "Hello" });
 }
