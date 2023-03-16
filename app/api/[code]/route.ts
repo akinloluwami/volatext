99ecaa5a40d457fceabf75a0b33d7910179596cc
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 11:59:35 +0100

    any

--- app/api/[code]/route.ts
+++ app/api/[code]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: Request, { params }: any) {
+  const code = params.code;
   return NextResponse.json({ hi: "Hello" });
 }
