ac45193027b265335ae55264f6e1b54901ca23ad
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 15:41:26 +0100

    ???

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="px-5 w-full py-4 flex justify-between items-center">
+    <div className="px-8 w-full py-5 flex justify-between items-center">
       <Link href={"/"}>
         <h1 className="text-xl font-semibold">Volatext</h1>
       </Link>
-      <button className="btn">Moon</button>
+      {/* <button className="btn">Moon</button> */}
     </div>
   );
 };
