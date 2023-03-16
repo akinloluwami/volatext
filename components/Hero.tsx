9429b25609e4627b566eb3798d229f71e42884b6
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 02:12:01 +0100

    ___

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="px-24 my-4">
+    <div className="px-24 my-4 flex items-center">
       <div className="w-1/2">
         <h1 className="text-8xl font-semibold">Securely share texts online</h1>
         <p className="font-semibold text-lg my-4">
@@ -11,7 +11,13 @@ const Hero = () => {
         </p>
         <button className="btn text-xl">Type & Share</button>
       </div>
-      <div className="w-12"></div>
+      <div className="w-1/2 ml-20">
+        <img
+          className=""
+          src="https://media3.giphy.com/media/fmkYSBlJt3XjNF6p9c/giphy.gif"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
