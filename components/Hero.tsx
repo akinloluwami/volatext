4e91f4a2a9c073275c1f1a0ef41b43c956f79f87
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 01:59:52 +0100

    struct

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Securely share texts online</h1>
+      <p>
+        Your text is encrypted and in our database and automatically deleted
+        after 15 minutes.
+      </p>
+      <button>Type & Share</button>
+    </div>
+  );
+};
+
+export default Hero;
