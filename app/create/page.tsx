42f226b4a3535349a34ac2e3db82250718fb7395
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 09:52:46 +0100

    ____

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import React from "react";
 
 const Create = () => {
+  const createText = () => {
+    fetch("/api/create", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="my-4  flex-col flex items-center justify-center">
       <textarea
         className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
         placeholder="Type here..."
       ></textarea>
-      <button className="btn my-4 lg:w-fit lg:px-20 w-full">Create</button>
+      <button
+        className="btn my-4 lg:w-fit lg:px-20 w-full"
+        onClick={createText}
+      >
+        Create
+      </button>
     </div>
   );
 };
