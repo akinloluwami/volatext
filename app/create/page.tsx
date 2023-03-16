3f17ce5b5e37b9a1f78dd8ed5369cc6464c3ad34
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 14:11:44 +0100

    success message

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -48,9 +48,7 @@ const Create = () => {
       <input type="checkbox" id="my-modal-6" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
+          <h3 className="font-bold text-lg">Text link created successfully</h3>
           <p className="py-4">
             You've been selected for a chance to get one year of subscription to
             use Wikipedia for free!
