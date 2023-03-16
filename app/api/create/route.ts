6a21300ba55313675d12d08c33423ceb491b2366
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 09:43:00 +0100

    ___

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,4 +1,4 @@
-import client from "../../../prisma/client";
+import client from "../../../prisma/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
