2d1508d614eb385978db0c6c7f57c252e0546c97
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 09:24:45 +0100

    ___

---
+++ app/api/create/route.ts
@@ -0,0 +1,19 @@
+import client from "../../../prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    client.$connect();
+    const Text = await client.text.create({
+      data: {
+        expiry_time: new Date(),
+        text: "aaaaaaaaaaaaa",
+        sharing_code: "2222",
+      },
+    });
+
+    return NextResponse.json({ message: "Success", Text });
+  } catch (error) {
+    return NextResponse.json({ message: "Something went wrong", error });
+  }
+}
