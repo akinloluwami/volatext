f5bb6ae2db3589d1a7c96d27355a2a2aa16601f8
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 02:07:48 +0100

    some styling...

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Securely share texts online</h1>
-      <p>
-        Your text is encrypted and in our database and automatically deleted
-        after 15 minutes.
-      </p>
-      <button>Type & Share</button>
+    <div className="px-24 my-4">
+      <div className="w-1/2">
+        <h1 className="text-8xl font-semibold">Securely share texts online</h1>
+        <p className="font-semibold text-lg my-4">
+          Texts are encrypted and in our database and automatically deleted
+          after 15 minutes.
+        </p>
+        <button className="btn text-xl">Type & Share</button>
+      </div>
+      <div className="w-12"></div>
     </div>
   );
 };
