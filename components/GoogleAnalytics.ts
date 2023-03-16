eefd7c6bd5f3de4a900ce835ed4cc8735ad98737
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 19:20:25 +0100

    ___

---
+++ components/GoogleAnalytics.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { initGA, logPageView } from "../utils/analytics";
+
+const GoogleAnalytics = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    initGA();
+  }, []);
+
+  useEffect(() => {
+    logPageView();
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router.events]);
+};
+
+export default GoogleAnalytics;
