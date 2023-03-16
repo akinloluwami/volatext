cb63d339490b1f829a6829e261d9e6caf93de0ca
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 13:19:02 +0100

    removed placeholder

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const TextPage = () => {
   return (
     <div className="my-4  flex-col flex items-center justify-center">
-      <textarea
-        className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
-        placeholder="Type here..."
-      ></textarea>
+      <textarea className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"></textarea>
       <button className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed">
         Copy text
       </button>
