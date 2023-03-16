052abbf5930879792175357d8020c8171a1684d6
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 09:25:00 +0100

    ____

--- app/api/getTexts/route.ts
+++ app/api/getTexts/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
     const texts = await client.text.findMany();
-    return NextResponse.json(texts);
+    return NextResponse.json({ texts });
   } catch (error) {
     return NextResponse.json({ message: "Something went wrong" });
   }
