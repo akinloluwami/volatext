148c78d44759d58c5e12c524599e61dae6af27a1
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 17:35:00 +0100

    loading animation___

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -32,7 +32,12 @@ const TextPage = ({ params: { code } }: { params: { code: string } }) => {
   return (
     <>
       {isLoading ? (
-        <div>Loading...</div>
+        <div className="mt-10">
+          <p className="text-center">Decrypting...</p>
+          <h1 className="text-center font-bold text-[20rem] animate-pulse">
+            *
+          </h1>
+        </div>
       ) : text.text ? (
         <div className="my-4 flex-col flex items-center justify-center">
           <div className="lg:w-1/2 w-full my-5">
@@ -56,7 +61,9 @@ const TextPage = ({ params: { code } }: { params: { code: string } }) => {
       ) : (
         <div className="flex my-20 items-center justify-center w-full flex-col">
           <h1 className="font-semibold text-9xl text-center">404</h1>
-          <p className="text-2xl">No text exist with this sharing code.</p>
+          <p className="text-2xl text-center">
+            No text exist with this sharing code.
+          </p>
           <Link href={"/create"}>
             <button className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed">
               Create new
