3c5922a4a7d9db1303fc533dcce2877b1fa3211f
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 02:42:19 +0100

    ___

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,10 +4,10 @@ const Create = () => {
   return (
     <div className="my-4  flex-col flex items-center justify-center">
       <textarea
-        className="p-2 bg-transparent border-2 border-gray-900 w-1/2 h-96 rounded-md"
+        className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
         placeholder="Type here..."
       ></textarea>
-      <button className="btn my-4">Create</button>
+      <button className="btn my-4 lg:w-fit lg:px-20 w-full">Create</button>
     </div>
   );
 };
