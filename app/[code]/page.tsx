f89c130b57445dcd510710ba3b01f60cb9911907
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 13:32:19 +0100

    ___

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -9,16 +9,22 @@ const TextPage = async ({ params: { code } }: { params: { code: string } }) => {
   const textData = getText(code);
   const [text] = await Promise.all([textData]);
   return (
-    <div className="my-4  flex-col flex items-center justify-center">
-      <textarea
-        className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
-        readOnly
-        value={text.text}
-      ></textarea>
-      <button className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed">
-        Copy text
-      </button>
-    </div>
+    <>
+      {text.text ? (
+        <div className="my-4  flex-col flex items-center justify-center">
+          <textarea
+            className="p-2 bg-transparent border-2 border-gray-900 lg:w-1/2 w-full h-96 rounded-md"
+            readOnly
+            value={text.text}
+          ></textarea>
+          <button className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed">
+            Copy text
+          </button>
+        </div>
+      ) : (
+        <div className="">404</div>
+      )}
+    </>
   );
 };
 
