5d24c87203cc4f7b0bb1bec3ff5f21fb1d9442da
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 14:39:04 +0100

    __

--- components/DynamicButton.tsx
+++ components/DynamicButton.tsx
@@ -1,10 +1,13 @@
+import copyToClipboard from "@/utils/copyToClipboard";
 import React from "react";
 
 const DynamicButton = ({ text }: { text: string }) => {
   return (
     <button
       className="btn my-4 lg:w-fit lg:px-20 w-full disabled:cursor-not-allowed"
-      onClick={}
+      onClick={() => {
+        copyToClipboard(text);
+      }}
     >
       Copy text
     </button>
