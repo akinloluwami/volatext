cbbf74abd78f35e8a6686bacc60c7ee046792a02
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-16 04:25:16 +0100

    try catch

--- app/api/getTexts/route.ts
+++ app/api/getTexts/route.ts
@@ -2,6 +2,10 @@ import client from "../../../prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const texts = await client.text.findMany();
-  return NextResponse.json(texts);
+  try {
+    const texts = await client.text.findMany();
+    return NextResponse.json(texts);
+  } catch (error) {
+    return NextResponse.json({ message: "Something went wrong" });
+  }
 }
