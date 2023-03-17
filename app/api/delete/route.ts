e24807258086de38364271d87880633a4e96d96d
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 14:58:31 +0100

    ___

---
+++ app/api/delete/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/prisma/prisma";
+import cryptr from "@/utils/cryptr";
+import dayjs from "dayjs";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const texts = await prisma.text.findMany();
+
+  const expired = texts.filter((text) => {
+    const now = dayjs();
+    const expiry = dayjs(text.expiry);
+    const diff = expiry.diff(now, "m");
+    return diff < 1;
+  });
+
+  if (expired.length === 0) {
+    return NextResponse.json({
+      message: "There are no expired messages",
+    });
+  }
+
+  expired.forEach(async (exp) => {
+    await prisma.text.delete({
+      where: {
+        id: exp.id,
+      },
+    });
+  });
+
+  return NextResponse.json({
+    message: `All ${expired.length} expired messages successfully deleted`,
+  });
+}
