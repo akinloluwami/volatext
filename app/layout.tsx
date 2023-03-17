0898e5ebf8c79b18fa585ec569766756f2c73fd9
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 13:43:29 +0100

    mfs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import * as gtag from "../utils/gtag";
 
 export const metadata = {
   title: "Volatext - Securely share texts online",
@@ -15,26 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <title>Volatext - Securely share texts online</title>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
-          }}
-        />
-      </head>
       <body>
         <Navbar />
         <div className="px-5">
