4529f9080e55a26b144ade4d41d8a1f30cfc66b6
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-17 01:54:19 +0100

    _

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-0CNHFYF21Y";
+export const GA_TRACKING_ID = "G-VH3QB95NRZ";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
