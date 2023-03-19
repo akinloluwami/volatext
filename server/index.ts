3aa54cabece4246242261a423545a32e774d0b52
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-19 15:12:53 +0100

    ____

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import prisma from "./prisma/prisma";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+app.get("/", async (req: Request, res: Response) => {
+  return res.send("Volatext");
+});
+
+app.listen(5656, () => console.log("Server is live"));
