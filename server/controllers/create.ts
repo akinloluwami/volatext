2c7190642b08300420470de45719669149e89d6f
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-19 15:18:34 +0100

    ...

---
+++ server/controllers/create.ts
@@ -0,0 +1,50 @@
+import dayjs from "dayjs";
+import randomstring from "randomstring";
+import cryptr from "@/utils/cryptr";
+import prisma from "../prisma/prisma";
+import { Request, Response } from "express";
+
+export async function create(req: Request, res: Response) {
+  try {
+    const { text, length, password, isProtected } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ message: "Text is required" });
+    }
+
+    if (isProtected && !password) {
+      return res.status(400).json({
+        message: "Password is require for protected content",
+      });
+    }
+
+    const expiry = dayjs()
+      .add(length || 15, length < 2 ? "minute" : "minutes")
+      .toDate();
+    const created = dayjs().toDate();
+
+    const encryptedString = cryptr.encrypt(text);
+
+    const Text = await prisma.text.create({
+      data: {
+        text: encryptedString,
+        sharing_code: randomstring.generate(4).toLowerCase(),
+        created,
+        expiry,
+        password: cryptr.encrypt(password),
+        isProtected,
+      },
+    });
+
+    await prisma.create.create({
+      data: {
+        created,
+      },
+    });
+
+    return res.status(201).json({ message: "Success", Text });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong", error });
+  }
+}
