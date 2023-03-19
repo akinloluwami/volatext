79c80d2c402f9bace29084d4e01ef08a8f35e77e
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-19 10:21:19 +0100

    try catch

--- app/api/decrypt/route.ts
+++ app/api/decrypt/route.ts
@@ -1,4 +1,12 @@
+import { NextResponse } from "next/server";
+
 export async function GET(
   request: Request,
   { params }: { params: { code: string; password: string } }
-) {}
+) {
+  try {
+    const { code, password } = params;
+  } catch (error) {
+    return NextResponse.json({ message: "Something went wrong" });
+  }
+}
