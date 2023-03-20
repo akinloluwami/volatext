2d064c8c170a513a34884a3d5a2175c0e91f06ba
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-20 17:01:19 +0100

    added self-destruct switch

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -86,6 +86,17 @@ const Create = () => {
             />
           )}
         </div>
+        <div className="w-full mt-5">
+          <div className="flex items-center gap-2">
+            <h2 className="font-semibold">Self-destruct</h2>
+            <input
+              type="checkbox"
+              className="toggle toggle-sm"
+              checked={selfDestruct}
+              onChange={() => setSelfDestruct(!selfDestruct)}
+            />
+          </div>
+        </div>
       </div>
 
       <label htmlFor="my-modal-6" className="btn hidden" ref={modalRef}>
